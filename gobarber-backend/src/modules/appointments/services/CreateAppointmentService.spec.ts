adf4c13bad081fd601caafc6ac3261c20159315f
Rodrigo Aguiar <rodsilva.aguiar@gmail.com>
2021-05-02 00:30:15 -0300

    Testes e TDD (week 4; day 5;)
    - Coverage report
    - Testes de agendamento
    - Testando criação de usuário

--- gobarber-backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ gobarber-backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,5 @@
+import AppError from '@shared/errors/AppError';
+
 import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -15,7 +17,20 @@ describe('CreateAppointment', () => {
     expect(appointment.provider_id).toBe('123456');
   });
 
-  // it('should not be able to create two appointments on the same time', () => {
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentRepository();
+    const createAppointmentService = new CreateAppointmentService(fakeAppointmentRepository);
 
-  // });
+    const date = new Date(2020, 4, 10, 11);
+
+    await createAppointmentService.execute({
+      date,
+      provider_id: '123456',
+    });
+
+    expect(createAppointmentService.execute({
+      date,
+      provider_id: '123456',
+    })).rejects.toBeInstanceOf(AppError);
+  });
 });
